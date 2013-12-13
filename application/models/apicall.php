0ffcdb6580ee0074f1747c8accf6799ab31202b3
Arnaud Rivard <arnaud.rivard@gmail.com>
2013-12-13 17:41:57 -0500

    ApiCall Functions
    Functions added to the apicall.php and defines.php created

--- application/models/apicall.php
+++ application/models/apicall.php
@@ -9,4 +9,65 @@
 		$values = json_decode($output,TRUE);
 		return $values;
 	}
+
+	public function getAllChampions($region)
+	{
+		$url = URL.URLPOSTFIX.$region.SLASH.VERSIONONE.CHAMPION;
+		$result = getarray($url);
+		return $result;
+	}
+
+	public function getRecentGamesById($region,$summonerId)
+	{
+		$url=URL.URLPOSTFIX.$region.SLASH.VERSIONONE.GAME.BYSUMMONER.$summonerId.SLASH.RECENT;
+		$result = getarray($url);
+		return $result;
+	}
+
+	public function getLeaguesDataById($region,$summonerId)
+	{
+		$url=URL.URLPOSTFIX.$region.SLASH.VERSIONTWO.LEAGUE.BYSUMMONER.$summonerId;
+		$result = getarray($url);
+		return $result;
+	}
+
+	public function getStatSummaryById($region,$summonerId)
+	{
+		$url=URL.URLPOSTFIX.$region.SLASH.VERSIONONE.STATS.BYSUMMONER.$summonerId.SLASH.SUMMARY;
+		$result = getarray($url);
+		return $result;
+	}
+
+	public function getRankedStatsById($region,$summonerId)
+	{
+		$url=URL.URLPOSTFIX.$region.SLASH.VERSIONONE.STATS.BYSUMMONER.$summonerId.SLASH.RANKED;
+		$result = getarray($url);
+		return $result;
+	}
+
+	public function getMasteriesById($region,$summonerId)
+	{
+		$url=URL.URLPOSTFIX.$region.SLASH.VERSIONONE.SUMMONER.$summonerId.SLASH.MASTERIES;
+		$result = getarray($url);
+		return $result;
+	}
+
+	public function getRunesById($region,$summonerId)
+	{
+		$url=URL.URLPOSTFIX.$region.SLASH.VERSIONONE.SUMMONER.$summonerId.SLASH.RUNES;
+		$result = getarray($url);
+		return $result;
+	}
+
+	public function getSummonersListByIds($region,$summonerIds[])
+	{
+
+	}
+
+	public function getTeamsFromId($region,$summonerId)
+	{
+		$url=URL.URLPOSTFIX.$region.SLASH.VERSIONTWO.TEAM.BYSUMMONER.$summonerId;
+		$result = getarray($url);
+		return $result;
+	}
 ?>	
