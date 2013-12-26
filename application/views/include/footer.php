0e78b5a5eaebd26ebfc166b33a8168f2219dc579
Guillaume Fortin <xguillaumex@hotmail.com>
2013-12-26 17:19:55 -0500

    footer fix

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -5,7 +5,7 @@
   		</div>
 		<!-- Load js at end of doc for faster loads -->
 	    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-	    <script src="assets/js/bootstrap.min.js"></script>
-	    <script src="assets/js/custom.js"></script>
+	    <script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
+	    <script src="<?php echo base_url('assets/js/custom.js'); ?>"></script>
 	</body>
 </html>
