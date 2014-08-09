50d6a535b7cb2ff0bbb1e1a605484038a9f3ff3d
philipblaquiere <philipblaquiere@gmail.com>
2014-08-09 18:39:07 -0400

    League Model Clean up, League search started
    Still broke : Player Recent Matches

--- application/models/season_model.php
+++ application/models/season_model.php
@@ -7,74 +7,22 @@ class Season_model extends MY_Model {
 		$this->db1 = $this->load->database('default', TRUE);
 	}
 
-  	public function get_season_by_name($seasonname) {
-	  	$sql = "SELECT * FROM seasons
-	  			WHERE name = '$seasonname' 
-	  			LIMIT 1";
-	  	$result = $this->db1->query($sql);
-		return $result->row_array();
-  	}
+	public function start_season($seasonid, $start_date, $end_date, $teams)
+  {
+		$sql = "UPDATE seasons
+			SET season_status = 'active', start_date = '$start_date', end_date = '$end_date'
+			WHERE seasonid = '$seasonid'";
 
-  	public function get_season_by_seasonid($sid) {
-	  	$sql = "SELECT * FROM seasons
-	  			WHERE seasonid = '$sid'
-	  			LIMIT 1";
-	  	$result = $this->db1->query($sql);
-		return $result->row_array();
-  	}
-
-  	public function get_season_dates($sid) {
-	  	$sql = "SELECT startdate as startdate, enddate as enddate FROM seasons
-	  			WHERE seasonid = '$sid' 
-	  			LIMIT 1";
-	  	$result = $this->db1->query($sql);
-		return $result->row_array();
-  	}
-
-  	public function get_current_season($teamid) {
-  		$sql = "SELECT s.start_date as start_date, s.season_esportid as season_esportid, s.season_status as season_status, s.season_status as season_status, s.created as created FROM seasons s
-	  			INNER JOIN season_leagues sl ON s.seasonid = sl.seasonid
-	  			INNER JOIN league_teams lt ON lt.leagueid = sl.leagueid
-	  			WHERE lt.teamid = '$teamid'
-	  			LIMIT 1";
-	  	$result = $this->db1->query($sql);
-		return $result->row_array();
-  	}
-
-
-  	public function create_season($season) {
-  		$uniqueid = $this->generate_unique_key();
-  		$sql = "INSERT INTO seasons (seasonid, owner_UserId, season_duration)
-  				VALUES ('" . $uniqueid . "', '" . $season['UserId'] . "', '" . $season['season_duration'] . "')";
-  		$this->db1->query($sql);
-  	}
-
-  	public function start_season($seasonid, $start_date, $end_date, $teams)
+    $sql2 = "INSERT INTO season_teams(seasonid, teamid) VALUES ";
+    foreach ($teams as $teamid)
     {
-  		$sql = "UPDATE seasons
-  			SET season_status = 'active', start_date = '$start_date', end_date = '$end_date'
-  			WHERE seasonid = '$seasonid'";
+      $sql2 .= "('" . $seasonid ."','".$teamid ."'),";
+    }
+    $sql2 = substr($sql, 0, -1);
 
-      $sql2 = "INSERT INTO season_teams(seasonid, teamid) VALUES ";
-      foreach ($teams as $teamid)
-      {
-        $sql2 .= "('" . $seasonid ."','".$teamid ."'),";
-      }
-      $sql2 = substr($sql, 0, -1);
-
-      $this->db1->trans_start();
-  		$this->db1->query($sql);
-      $this->db1->query($sql2);
-      $this->db1->trans_complete();
-  	}
-
-  	public function get_seasons_by_owner($userid, $esportid) {
-  		$sql = "SELECT * FROM seasons
-  				WHERE owner_userid = '$userid'
-  				AND season_status = 'active' OR season_status = 'new'
-          AND season_esportid = '$esportid'
-  				LIMIT 1";
-  		$result = $this->db1->query($sql);
-		return $result->row_array();
-  	}
+    $this->db1->trans_start();
+		$this->db1->query($sql);
+    $this->db1->query($sql2);
+    $this->db1->trans_complete();
+	}
 }
--- application/views/view_leagues.php
+++ application/views/view_leagues.php
@@ -6,137 +6,16 @@
   <?php } ?>
 </div>
 <!-- Header -->
-<div name="league_search_parameters_container">
-	<div class="input-group col-sm-8">
- 		<?php echo validation_errors(); ?>
- 		<div class="panel-group " id="accordion">
-			<div class="panel panel-default">
-				<?php echo form_open('search_league', array('class' => 'form-horizontal', 'id' => 'registrationForm')); ?>
-					<div class="panel-heading">
-						<h6 class="panel-title">
-							<div class="input-group">
-								<?php echo form_input(array('name' => 'name', 'class' => 'form-control', 'placeholder' => 'Search', 'value' => set_value('search_text'))); ?>
-								<span class="input-group-btn">
-									<a data-toggle="collapse" data-parent="#accordion" class="btn btn-default" role="button" href="#collapseOne">
-								        <span class="glyphicon glyphicon-filter"></span>
-								    </a>
-								</span>
-							</div>
-						</h6>
-					</div>
-					<div id="collapseOne" class="panel-collapse collapse">
-						<div class="panel-body">
-							<div class="refine_search_parameters">
-								<h4>Refine your search:</h4>
-								<div class="input-group">
-									<label class="checkbox-inline"><input type="checkbox" id="leaguenotfullcheckbox" name="leaguenotfullcheckbox" value="leaguenotfullcheckbox">League Not Full</label>
-								</div>
-								<div class="input-group">
-									<label class="checkbox-inline"><input type="checkbox" id="leaguenotfullcheckbox" name="leaguenotfullcheckbox" value="leaguenotfullcheckbox">League Not Empty</label>
-								</div>
-								<div class="input-group">
-									<label class="checkbox-inline"><input type="checkbox" id="inviteonlycheckbox" name="inviteonlycheckbox" value="inviteonlycheckbox">I can Join</label>
-								</div>
-								<div class="input-group">
-									<label class="checkbox-inline"><input type="checkbox" id="inviteonlycheckbox" name="inviteonlycheckbox" value="inviteonlycheckbox">Invite Only</label>
-								</div>
-								<div class="input-group">
-									<label class="checkbox-inline">
-										<input type="checkbox" id="mondaycheckbox" name="mondaycheckbox" value="mondaytimepicker">Mondays
-									</label>
-									<label class="checkbox-inline">
-										<input type="checkbox" id="tuesdaycheckbox" name="tuesdaycheckbox" value="tuesdaytimepicker">Tuesdays
-									</label>
-									<label class="checkbox-inline">
-										<input type="checkbox" id="wednesdaycheckbox" name="wednesdaycheckbox" value="wednesdaytimepicker">Wednesdays
-									</label>
-									<label class="checkbox-inline">
-										<input type="checkbox" id="thursdaycheckbox" name="thursdaycheckbox" value="thursdaytimepicker">Thursdays
-									</label>
-									<label class="checkbox-inline">
-										<input type="checkbox" id="fridaycheckbox" name="fridaycheckbox" value="fridaytimepicker">Fridays
-									</label>
-									<label class="checkbox-inline">
-										<input type="checkbox" id="saturdaycheckbox" name="saturdaycheckbox" value="saturdaytimepicker">Saturdays
-									</label>
-									<label class="checkbox-inline">
-										<input type="checkbox" id="sundaycheckbox" name="sundaycheckbox" value="sundaytimepicker">Sundays
-									</label>
-								</div>
-								<br/>
-								<div class="form-group">
-								    <div class="col-sm-2 input-append bootstrap-timepicker">
-								    	<input name="leaguestarttime" value="12:00 PM"  type="text" class="form-control timepicker">
-								    </div>
-								    <div class="col-sm-2 input-append bootstrap-timepicker">
-								    	<input name="leagueendtime" value="12:00 PM"  type="text" class="form-control timepicker">
-								    </div>
-							    </div>
-							</div>
-						</div>
-					</div>
-				<?php echo form_close(); ?>
-			</div>
-		</div>
-	</div>
-</div>
+	
+<?php echo form_input(array('name' => 'search_leagues', 'data-toggle' => 'hide-seek', 'id' => 'search_leagues', 'class' => 'form-control', 'data-list' =>'.default_list', 'placeholder' => 'Search', 'autocomplete' => 'off')); ?>
 <br/>
-<div class="panel panel-default">
-	<div class="panel-heading">Leagues</div>
-	<div class="panel-body">
-		<div class="list-group">
-			<div id="league_search_results">
-				<?php if(count($leagues) > $max_league_count) { 
-					$num_pages = ceil((count($leagues)/ $max_league_count));
-					for ($i = 1; $i <= $num_pages; $i++) { } ?>
-					<ul class="pagination">
-						<li><a href="#">&laquo;</a></li>
-							<?php for ($j = 1; $j <= $num_pages; $j++) { ?>
-										<li><a href="#"><?php echo $j ?></a></li>
-							<?php } ?>
-						<li><a href="#">&raquo;</a></li>	
-					</ul>
-				<?php } 
-				else { 
-				  foreach($leagues as $league): ?>
-					<div class="list-group-item">
-						<div class="row">
-							<div class="col-md-10">
-				                <p class="list-group-item-text"><a href="<?php echo site_url('leagues/view/' . $league['leagueid']) ?>"><?php echo $league['league_name']?></a><p>
-				                <p class="list-group-item-text"><?php echo $league['invite'] == 1 ? "Invite Only" : null ?></p>
-				                <!--<p class="list-group-item-text">Games/Week: <?php echo count($league['first_matches']) ?></p>-->
-				                <p class="list-group-item-text">Teams: <?php echo $league['num_teams'] . "/" . $league['max_teams'] ?></p>
-				                <!--<p class="list-group-item-text">
-				                	<?php foreach ($league['first_matches'] as $first_game) : ?>
-					        			<?php echo date("D'\s \- h:i A.",strtotime($first_game)) ?>
-					        		<?php endforeach; ?></p>-->
-					        	<p class="list-group-item-text">Tooltip: <?php echo $league['join_status_tooltip'] ?></p>
-					        </div>
-					        <div class="col-md-2">
-					        	<div class="btn-toolbar " role="toolbar">
-				              		<div class="btn-group">
-				              			<a href="#" type="button" class="btn btn-default" role="button">
-				              				<span class="glyphicon glyphicon-pencil"></span>
-				              			</a>
-				              			<?php if($league['can_join']) { ?>
-					              			<a href="<?php echo site_url('leagues/join/' . $league['leagueid']) ?>" type="button" class="btn btn-default" role="button">
-					              				<?php echo $league['join_status'] ?>
-					              			</a>
-				              			<?php }
-				              			else { ?>
-					              			<a type="button" disabled class="btn btn-default" role="button">
-					              				<?php echo $league['join_status'] ?>
-					              			</a>
-				              			<?php } ?>
-				              		</div>
-					            </div>
-					        </div>
-				    	</div>   	
-				    </div> 
-				<?php endforeach; 
-				} ?>
-			</div>
-		</div>
-	</div>
+<div class="list-group">
+	<ul class="list-group default_list">
+		<?php foreach($leagues as $league): ?>
+		<li  class="list-group-item">
+            <a href="<?php echo site_url('leagues/view/' . $league['leagueid']) ?>"><?php echo $league['league_name']?></a>
+	    </li>
+		<?php endforeach; ?>
+	</ul>
 </div>
 
