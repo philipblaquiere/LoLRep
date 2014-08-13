21f5e88ab343599ba8cb702e7e4e8b87951e3ae4
philipblaquiere <philipblaquiere@gmail.com>
2014-08-12 20:11:09 -0400

    Fix: Leagues didn't automatically load b4 search

--- application/views/league_list.php
+++ application/views/league_list.php
@@ -1,11 +1,9 @@
-<div class="list-group">
-	<ul class="default_list">
-		<?php foreach($leagues as $league): ?>
-		<li class="list-group-item">
-            <h3><a href="<?php echo site_url('leagues/view/' . $league['leagueid']) ?>"><?php echo $league['league_name']?></a></h3>
-            <p><?php echo $league['league_type']?></p>
-            <p><?php echo isset($league['seasons'][$league['current_season']]['teams']) ? count($league['seasons'][$league['current_season']]['teams']) : 0 ?>/<?php echo $league['max_teams'] ?> teams</p>
-	    </li>
-		<?php endforeach; ?>
-	</ul>
-</div>
+<ul class="list-group">
+	<?php foreach($leagues as $league): ?>
+	<li class="list-group-item">
+        <h3><a href="<?php echo site_url('leagues/view/' . $league['leagueid']) ?>"><?php echo $league['league_name']?></a></h3>
+        <p><?php echo $league['league_type']?></p>
+        <p><?php echo isset($league['seasons'][$league['current_season']]['teams']) ? count($league['seasons'][$league['current_season']]['teams']) : 0 ?>/<?php echo $league['max_teams'] ?> teams</p>
+    </li>
+	<?php endforeach; ?>
+</ul>
